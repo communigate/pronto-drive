703ef05f962173a9a8e843f733f211017ca111ab
Anton Katsarov <anton@webface.bg>
2014-06-12 14:21:22 +0300

    * Purged some cPanel related files

--- scripts/install_cpanel_module.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/local/cpanel/3rdparty/bin/perl
-
-use CPAN;
-exit unless $ARGV[0];
-my $missing = system("/usr/local/cpanel/3rdparty/bin/perl -M$ARGV[0] -e 1 2>/dev/null");
-if ($missing) {
-    CPAN::Shell->force("install",$ARGV[0]);
-}
--- scripts/unregister_apps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PACKSRC=`pwd`
-
-for i in `ls /var/cpanel/apps/addon_cgpro*.conf`; do
-    /usr/local/cpanel/bin/unregister_appconfig "$i";
-done;
