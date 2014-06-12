6ea542a6c3e06eee33c39bae0141c755c707e1b9
Anton Katsarov <anton@webface.bg>
2014-06-12 15:58:18 +0300

    * Reverted login.wssp * bugFixed innstall.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ source ${PACKSRC}/config.ini
 
 
 # Install WebSkins
+mkdir -p /var/CommuniGate/WebSkins
 cp -r ${PACKSRC}/WebSkins/* /var/CommuniGate/WebSkins/
+mkdir -p /var/CommuniGate/cgi/
 chmod +x ${PACKSRC}/cgi/*
 cp ${PACKSRC}/cgi/* /var/CommuniGate/cgi/
 
