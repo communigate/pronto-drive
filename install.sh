a17b8f0592e8cfee4a5abf6e566a54e963dab5ab
Anton Katsarov <anton@webface.bg>
2014-06-24 16:49:53 +0300

    * Added version check for perl dependencies

--- install.sh
+++ install.sh
@@ -13,3 +13,51 @@ cp ${PACKSRC}/cgi/* /var/CommuniGate/cgi/
 
 service CommuniGate stop
 service CommuniGate start
+
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+if [ `perl -MGD::Barcode::QRcode -e 1 2>&1 | wc -l` -ne "0" ]
+then
+    echo "!!! Please install perl module GD::Barcode::QRcode !!!";
+fi
+if [ `perl -MIO::Compress::Zip -e 1 2>&1 | wc -l` -ne "0" ]
+then
+    echo "!!! Please install perl module IO::Compress::Zip !!!";
+else
+   VERSION=`perl -MIO::Compress::Base -e 'print $IO::Compress::Base::VERSION'`;
+   vercomp "2.040" $VERSION;
+   if [ $? == 1 ]
+   then
+       echo "!!! Please install perl module IO::Compress::Zip with version at least 2.040 !!!";
+   fi;
+fi
+
